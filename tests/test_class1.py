d5869e95633f0410907f611939966290784c8b2d
Kirk Byers <ktbyers@twb-tech.com>
2019-05-31 13:11:14 -0700

    Class1 Tests

--- tests/test_class1.py
+++ tests/test_class1.py
@@ -100,3 +100,76 @@ def test_class1_ex3():
     assert "cisco3" in std_out
     assert "cisco_ios" in std_out
     assert std_err == ""
+
+
+def test_class1_ex4():
+    base_path = "../class1/exercises/exercise4/"
+    cmd_list = ["python", "exercise4.py"]
+
+    nornir_inventory = gen_inventory_dict(base_path)
+    nr = InitNornir(inventory=nornir_inventory, logging=NORNIR_LOGGING)
+    assert len(nr.inventory.hosts) == 2
+    for host_name, host_obj in nr.inventory.hosts.items():
+        assert host_obj.hostname is not None
+        assert len(host_obj.groups) == 1
+        assert host_obj.groups[0] == "ios"
+        assert host_obj.platform == "cisco_ios"
+        assert host_obj.username == "pyclass"
+        assert host_obj.password == "cisco123"
+        assert host_obj.port == 22
+
+    my_group = nr.inventory.groups["ios"]
+    assert my_group.hostname is None
+    assert my_group.platform == "cisco_ios"
+    assert my_group.port == 22
+
+    defaults = nr.inventory.defaults
+    assert defaults.username == "pyclass"
+    assert defaults.password == "cisco123"
+
+    std_out, std_err, return_code = subprocess_runner(cmd_list, exercise_dir=base_path)
+    assert return_code == 0
+    assert "cisco3.lasthop.io" in std_out
+    assert "cisco4.lasthop.io" in std_out
+    assert "These aren't the droids you're looking for" in std_out
+    assert std_err == ""
+
+
+def test_class1_ex5():
+    base_path = "../class1/exercises/exercise5/"
+    cmd_list = ["python", "exercise5.py"]
+
+    nornir_inventory = gen_inventory_dict(base_path)
+    nr = InitNornir(inventory=nornir_inventory, logging=NORNIR_LOGGING)
+
+    assert len(nr.inventory.hosts) == 2
+    for host_name, host_obj in nr.inventory.hosts.items():
+        assert host_obj.hostname is not None
+        assert len(host_obj.groups) == 1
+        assert host_obj.groups[0] == "ios"
+        assert host_obj.platform == "cisco_ios"
+        assert host_obj.username == "pyclass"
+        assert host_obj.password == "cisco123"
+        assert host_obj.port == 22
+        if host_name == "cisco3":
+            assert host_obj["dns1"] == "8.8.8.8"
+
+    my_group = nr.inventory.groups["ios"]
+    assert my_group.hostname is None
+    assert my_group.platform == "cisco_ios"
+    assert my_group.port == 22
+
+    defaults = nr.inventory.defaults
+    assert defaults.username == "pyclass"
+    assert defaults.password == "cisco123"
+    assert defaults.data["dns1"] == "1.1.1.1"
+    assert defaults.data["dns2"] == "1.0.0.1"
+
+    std_out, std_err, return_code = subprocess_runner(cmd_list, exercise_dir=base_path)
+    assert return_code == 0
+    assert "cisco3.lasthop.io" in std_out
+    assert "cisco4.lasthop.io" in std_out
+    assert "8.8.8.8" in std_out
+    assert "1.1.1.1" in std_out
+    assert "1.0.0.1" in std_out
+    assert std_err == ""
