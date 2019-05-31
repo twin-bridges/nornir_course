777d85e96382bf673b0317c92b26a19c88799f98
Kirk Byers <ktbyers@twb-tech.com>
2019-05-31 12:46:51 -0700

    Test code

--- tests/test_class1.py
+++ tests/test_class1.py
@@ -2,21 +2,17 @@ import subprocess
 from nornir import InitNornir
 import nornir
 
-NORNIR_LOGGING = {
-    "enabled": False,
-}
+NORNIR_LOGGING = {"enabled": False}
+
 
 def gen_inventory_dict(base_path):
     """Dynamically create an inventory dictionary using exercise path."""
     # BASE_PATH = "../class1/exercises/exercise1/"
-    NORNIR_HOSTS = f"{base_path}/hosts.yaml" 
-    NORNIR_GROUPS = f"{base_path}/groups.yaml" 
-    NORNIR_INVENTORY =  {
+    NORNIR_HOSTS = f"{base_path}/hosts.yaml"
+    NORNIR_GROUPS = f"{base_path}/groups.yaml"
+    NORNIR_INVENTORY = {
         "plugin": "nornir.plugins.inventory.simple.SimpleInventory",
-        "options": {
-            "host_file": NORNIR_HOSTS,
-            "group_file": NORNIR_GROUPS,
-        }
+        "options": {"host_file": NORNIR_HOSTS, "group_file": NORNIR_GROUPS},
     }
     return NORNIR_INVENTORY
 
@@ -41,10 +37,7 @@ def test_class1_ex1():
 
 def test_class1_ex2():
     base_path = "../class1/exercises/exercise2/"
-    cmd_list = [
-        'python',
-        'exercise2.py',
-    ]
+    cmd_list = ["python", "exercise2.py"]
 
     nornir_inventory = gen_inventory_dict(base_path)
     nr = InitNornir(inventory=nornir_inventory, logging=NORNIR_LOGGING)
@@ -52,17 +45,17 @@ def test_class1_ex2():
     for host_name, host_obj in nr.inventory.hosts.items():
         assert host_obj.hostname is not None
         assert len(host_obj.groups) == 1
-        assert host_obj.groups[0] == 'ios'
-        assert host_obj.platform == 'cisco_ios'
-        assert host_obj.username == 'pyclass'
-        assert host_obj.password == 'cisco123'
+        assert host_obj.groups[0] == "ios"
+        assert host_obj.platform == "cisco_ios"
+        assert host_obj.username == "pyclass"
+        assert host_obj.password == "cisco123"
         assert host_obj.port == 22
-   
+
     my_group = nr.inventory.groups["ios"]
     assert my_group.hostname is None
-    assert my_group.platform == 'cisco_ios'
-    assert my_group.username == 'pyclass'
-    assert my_group.password == 'cisco123'
+    assert my_group.platform == "cisco_ios"
+    assert my_group.username == "pyclass"
+    assert my_group.password == "cisco123"
     assert my_group.port == 22
 
     std_out, std_err, return_code = subprocess_runner(cmd_list, exercise_dir=base_path)
