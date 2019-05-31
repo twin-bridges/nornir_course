fa115019ebf994aee5d9ad687eede7c867da1937
Kirk Byers <ktbyers@twb-tech.com>
2019-05-31 13:00:15 -0700

    Class1 tests

--- tests/test_class1.py
+++ tests/test_class1.py
@@ -10,9 +10,14 @@ def gen_inventory_dict(base_path):
     # BASE_PATH = "../class1/exercises/exercise1/"
     NORNIR_HOSTS = f"{base_path}/hosts.yaml"
     NORNIR_GROUPS = f"{base_path}/groups.yaml"
+    NORNIR_DEFAULTS = f"{base_path}/defaults.yaml"
     NORNIR_INVENTORY = {
         "plugin": "nornir.plugins.inventory.simple.SimpleInventory",
-        "options": {"host_file": NORNIR_HOSTS, "group_file": NORNIR_GROUPS},
+        "options": {
+            "host_file": NORNIR_HOSTS,
+            "group_file": NORNIR_GROUPS,
+            "defaults_file": NORNIR_DEFAULTS,
+        },
     }
     return NORNIR_INVENTORY
 
@@ -57,6 +62,38 @@ def test_class1_ex2():
     assert my_group.username == "pyclass"
     assert my_group.password == "cisco123"
     assert my_group.port == 22
+
+    std_out, std_err, return_code = subprocess_runner(cmd_list, exercise_dir=base_path)
+    assert return_code == 0
+    assert "cisco3" in std_out
+    assert "cisco_ios" in std_out
+    assert std_err == ""
+
+
+def test_class1_ex3():
+    base_path = "../class1/exercises/exercise3/"
+    cmd_list = ["python", "exercise3.py"]
+
+    nornir_inventory = gen_inventory_dict(base_path)
+    nr = InitNornir(inventory=nornir_inventory, logging=NORNIR_LOGGING)
+    assert len(nr.inventory.hosts) == 2
+    for host_name, host_obj in nr.inventory.hosts.items():
+        assert host_obj.hostname is not None
+        assert len(host_obj.groups) == 1
+        assert host_obj.groups[0] == "ios"
+        assert host_obj.platform == "cisco_ios"
+        assert host_obj.username == "pyclass"
+        assert host_obj.password == "cisco123"
+        assert host_obj.port == 22
+
+    my_group = nr.inventory.groups["ios"]
+    assert my_group.hostname is None
+    assert my_group.platform == "cisco_ios"
+    assert my_group.port == 22
+
+    defaults = nr.inventory.defaults
+    assert defaults.username == "pyclass"
+    assert defaults.password == "cisco123"
 
     std_out, std_err, return_code = subprocess_runner(cmd_list, exercise_dir=base_path)
     assert return_code == 0
