5aa06c596292c3b69e88ed9fae26c677ad172313
carlniger@gmail.com <carlniger@gmail.com>
2019-06-19 15:15:32 -0700

    fix ex4 to match lesson

--- class2/exercises/exercise4/exercise4.py
+++ class2/exercises/exercise4/exercise4.py
@@ -1,8 +1,7 @@
 import os
-import re
 from nornir import InitNornir
 from nornir.core.filter import F
-from nornir.plugins.tasks.networking import netmiko_send_command
+from nornir.plugins.tasks.networking import napalm_get
 
 
 def main():
@@ -12,17 +11,14 @@ def main():
     ios_filt = F(groups__contains="ios")
     eos_filt = F(groups__contains="eos")
     nr = nr.filter(ios_filt | eos_filt)
-    my_results = nr.run(task=netmiko_send_command, command_string="show ip arp")
+    my_results = nr.run(task=napalm_get, getters=["arp_table"])
     parsed_results = []
     for host, data in my_results.items():
-        output = data[0].result
-        desired_data = [line for line in output.splitlines() if "10.220.88.1" in line][
-            0
-        ]
+        output = data[0].result["arp_table"]
+        desired_data = [line for line in output if line["ip"] == "10.220.88.1"][0]
         parsed_results.append((host, desired_data))
     for entry in parsed_results:
-        gw = re.sub(r"\s+", " ", entry[1])
-        print(f"Host: {entry[0]}, Gateway: {gw}")
+        print(f"Host: {entry[0]}, Gateway: {entry[1]}")
 
 
 if __name__ == "__main__":
