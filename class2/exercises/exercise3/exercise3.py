bc559afb01cd2fa8762e87f9645d45bef1dd7740
Kirk Byers <ktbyers@twb-tech.com>
2019-06-26 17:44:39 -0700

    Class2 exercise3

--- class2/exercises/exercise3/exercise3.py
+++ class2/exercises/exercise3/exercise3.py
@@ -7,22 +7,28 @@ from nornir.plugins.tasks.networking import netmiko_send_command
 
 def main():
     nr = InitNornir(config_file="config.yaml")
-    # Avoid storing lab password in GitHub, pull from environment variable
-    nr.inventory.defaults.password = os.environ["NORNIR_PASSWORD"]
+    # Filtering - this will be explained in detail in later lessons.
     ios_filt = F(groups__contains="ios")
     eos_filt = F(groups__contains="eos")
     nr = nr.filter(ios_filt | eos_filt)
     my_results = nr.run(task=netmiko_send_command, command_string="show ip arp")
+
     parsed_results = []
-    for host, data in my_results.items():
-        output = data[0].result
-        desired_data = [line for line in output.splitlines() if "10.220.88.1" in line][
-            0
-        ]
+    for host, multi_result in my_results.items():
+        # multi_result is a list and in this example only has one element
+        output = multi_result[0].result
+        desired_data = ""
+        for line in output.splitlines():
+            if "10.220.88.1" in line:
+                desired_data = line
+                break
         parsed_results.append((host, desired_data))
-    for entry in parsed_results:
-        gw = re.sub(r"\s+", " ", entry[1])
-        print(f"Host: {entry[0]}, Gateway: {gw}")
+
+    print()
+    for host, gw_data in parsed_results:
+        gw = re.sub(r"\s+", " ", gw_data)
+        print(f"Host: {host}, Gateway: {gw}")
+    print()
 
 
 if __name__ == "__main__":
