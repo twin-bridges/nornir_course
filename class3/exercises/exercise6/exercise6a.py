6124df2f0f84f7c61a201f8ce217eff3f92f101c
Kirk Byers <ktbyers@twb-tech.com>
2019-08-02 22:51:52 +0000

    Class3, ex6

--- class3/exercises/exercise6/exercise6a.py
+++ class3/exercises/exercise6/exercise6a.py
@@ -8,10 +8,7 @@ from nornir.plugins.functions.text import print_result
 def main():
     nr = InitNornir(config_file="config.yaml")
     nr = nr.filter(F(groups__contains="nxos"))
-    agg_result = nr.run(
-        task=napalm_get,
-        getters=["config"]
-    )
+    agg_result = nr.run(task=napalm_get, getters=["config"])
     print_result(agg_result)
 
 
