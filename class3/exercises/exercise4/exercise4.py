e57734ad27fa507749bbf456e56423fea259d3c1
Kirk Byers <ktbyers@twb-tech.com>
2019-08-02 20:48:06 +0000

    Class3, exercise4

--- class3/exercises/exercise4/exercise4.py
+++ class3/exercises/exercise4/exercise4.py
@@ -5,21 +5,36 @@ from nornir.plugins.tasks.networking import netmiko_send_command
 
 
 def main():
-    # Exercise 5a
+    # Exercise 4a
     nr = InitNornir(config_file="config.yaml")
-    # Exercise 5b
+    # Exercise 4b
     nr = nr.filter(F(groups__contains="eos"))
     result = nr.run(
         task=netmiko_send_command, command_string="show int status", use_textfsm=True
     )
+
+    # Verify structured data (pick a device and check)
+    print()
+    print("Exercise 4b - verify structured data")
+    print("-" * 20)
+    print(type(result['arista1'][0].result))
+    print("-" * 20)
+
+    print("\nExercise 4c - final dictionary")
+    print("-" * 20)
     combined_data = {}
-    for res in result:
-        combined_data[res] = {}
-        for interface in result[res].result:
-            combined_data[res][interface["port"]] = {}
-            combined_data[res][interface["port"]]["status"] = interface["status"]
-            combined_data[res][interface["port"]]["vlan"] = interface["vlan"]
+    for device_name, multi_result in result.items():
+        combined_data[device_name] = {}
+        device_result = multi_result[0]
+        for intf_dict in device_result.result:
+            intf_name = intf_dict["port"] 
+            inner_dict = {}
+            inner_dict["status"] = intf_dict["status"]
+            inner_dict["vlan"] = intf_dict["vlan"]
+            combined_data[device_name][intf_name] = inner_dict
     pprint(combined_data)
+    print("-" * 20)
+    print()
 
 
 if __name__ == "__main__":
