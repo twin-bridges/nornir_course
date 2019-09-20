670b1893140d8500ef2385a0d9baf59569bb325b
Kirk Byers <ktbyers@twb-tech.com>
2019-09-20 11:27:06 -0700

    Bonus1 exercise2

--- bonus1/exercises/exercise2/exercise2b.py
+++ bonus1/exercises/exercise2/exercise2b.py
@@ -10,36 +10,24 @@ PASSWORD = os.environ.get("NORNIR_PASSWORD", None)  # for testing purposes
 
 def transform_ansible_inventory(host):
     host.password = PASSWORD or host["ansible_ssh_pass"]
+    netmiko_params = host.get_connection_parameters("netmiko")
+    napalm_params = host.get_connection_parameters("napalm")
     if "nxos" in host.groups:
-        netmiko_params = host.get_connection_parameters("netmiko")
         netmiko_params.platform = "cisco_nxos"
-        host.connection_options["netmiko"] = netmiko_params
-        napalm_params = host.get_connection_parameters("napalm")
         napalm_params.platform = "nxos"
         napalm_params.port = "8443"
-        host.connection_options["napalm"] = napalm_params
     elif "cisco" in host.groups:
-        netmiko_params = host.get_connection_parameters("netmiko")
         netmiko_params.platform = "cisco_ios"
-        host.connection_options["netmiko"] = netmiko_params
-        napalm_params = host.get_connection_parameters("napalm")
         napalm_params.platform = "ios"
-        host.connection_options["napalm"] = napalm_params
     elif "arista" in host.groups:
-        netmiko_params = host.get_connection_parameters("netmiko")
         netmiko_params.platform = "arista_eos"
         netmiko_params.extras["global_delay_factor"] = 4
-        host.connection_options["netmiko"] = netmiko_params
-        napalm_params = host.get_connection_parameters("napalm")
         napalm_params.platform = "eos"
-        host.connection_options["napalm"] = napalm_params
     elif "juniper" in host.groups:
-        netmiko_params = host.get_connection_parameters("netmiko")
         netmiko_params.platform = "juniper_junos"
-        host.connection_options["netmiko"] = netmiko_params
-        napalm_params = host.get_connection_parameters("napalm")
         napalm_params.platform = "junos"
-        host.connection_options["napalm"] = napalm_params
+    host.connection_options["netmiko"] = netmiko_params
+    host.connection_options["napalm"] = napalm_params
 
 
 def main():
