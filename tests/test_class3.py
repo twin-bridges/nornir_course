374a1d833570a77d20ca0b5565aee67b82a2d2ba
Kirk Byers <ktbyers@twb-tech.com>
2020-05-20 14:41:16 -0700

    Fixing issue with class3 tests (incorrect running/startup count)

--- tests/test_class3.py
+++ tests/test_class3.py
@@ -99,8 +99,8 @@ def test_class3_ex6a():
     assert std_out.count("nxos2") == 5
     assert std_out.count("napalm_get") == 5
     assert std_out.count("candidate") == 2
-    assert std_out.count("running") == 4
-    assert std_out.count("startup") == 4
+    assert std_out.count("running") == 2
+    assert std_out.count("startup") == 2
     assert std_err == ""
 
 
@@ -115,7 +115,7 @@ def test_class3_ex6b():
     assert std_out.count("nxos2") == 3
     assert std_out.count("napalm_get") == 5
     assert std_out.count("candidate") == 2
-    assert std_out.count("running") == 4
+    assert std_out.count("running") == 2
     assert std_out.count("startup") == 2
     assert std_err == ""
 
@@ -132,7 +132,7 @@ def test_class3_ex6c():
     assert std_out.count("facts") == 2
     assert std_out.count("napalm_get") == 5
     assert std_out.count("candidate") == 2
-    assert std_out.count("running") == 4
+    assert std_out.count("running") == 2
     assert std_out.count("startup") == 2
     assert std_err == ""
 
