43c465da751fe7d2c51a8ec7a1ba8ccf75e226bb
Kirk Byers <ktbyers@twb-tech.com>
2020-07-08 10:37:14 -0700

    Update test script

--- tests.sh
+++ tests.sh
@@ -5,7 +5,16 @@
 # uncomment if overriding legacy EOS gateway:
 # export EOS_GATEWAY="10.0.2.2"
 
-pylama .
-black --check .
-cd tests
-py.test -s -v .
+RETURN_CODE=0
+
+echo "pylama ." \
+&& pylama . \
+&& echo "black" \
+&& black --check . \
+&& echo "running pytest..." \
+&& cd tests \
+&& py.test -s -v . \
+\
+|| RETURN_CODE=1
+
+exit $RETURN_CODE
