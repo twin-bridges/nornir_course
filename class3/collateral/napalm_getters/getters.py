9a4891615aeeb564bb63272bdb87c4a534f29c1d
Kirk Byers <ktbyers@twb-tech.com>
2021-10-15 14:29:38 -0700

    napalm getters content

--- class3/collateral/napalm_getters/getters.py
+++ class3/collateral/napalm_getters/getters.py
@@ -1,11 +1,36 @@
+import pdbr  # noqa
+from rich import print
 from nornir import InitNornir
+from nornir_napalm.plugins.tasks import napalm_get
+from nornir_utils.plugins.functions import print_result
 
 
 def main():
     nr = InitNornir(config_file="config.yaml")
-    print(nr)
-    # import ipdb
-    # ipdb.set_trace()
+
+    # pdbr.set_trace()
+
+    nr = nr.filter(name="srx2")
+    print(nr.inventory.hosts)
+
+    results = nr.run(task=napalm_get, getters=["config"])
+    print_result(results)
+
+    results = nr.run(task=napalm_get, getters=["config"], retrieve="running")
+    print_result(results)
+
+    results = nr.run(task=napalm_get, getters=["config", "facts"], retrieve="running")
+    print_result(results)
+
+    print(nr.data.failed_hosts)
+    nr.data.recover_host("srx2")
+
+    results = nr.run(
+        task=napalm_get,
+        getters=["config", "facts"],
+        getters_options={"config": {"retrieve": "running"}},
+    )
+    print_result(results)
 
 
 if __name__ == "__main__":
