2e446c34bbcc109d6a7dbeda22652358f7289ad6
Kirk Byers <ktbyers@twb-tech.com>
2021-11-17 15:44:30 -0800

    Ansible inventory updates

--- bonus1/collateral/ansible_inventory/ansible_inventory2.py
+++ bonus1/collateral/ansible_inventory/ansible_inventory2.py
@@ -6,14 +6,12 @@ from nornir_netmiko import netmiko_send_command
 
 def main():
     nr = InitNornir(config_file="config.yaml")
-    nr = nr.filter(name="nxos1")
-    # nxos = nr.inventory.hosts["nxos1"]
-    # Fix so that automated tests will work properly
     nr.inventory.hosts["nxos1"].password = os.environ["NORNIR_PASSWORD"]
-    agg_result = nr.run(
-        task=netmiko_send_command, command_string="show run | i hostname"
-    )
-    print(agg_result["nxos1"].result)
+
+    nxos = nr.inventory.hosts["nxos1"]
+    nxos_group = nr.inventory.groups["nxos"]
+    eos = nr.inventory.hosts["arista1"]
+    # pdbr.set_trace()
 
 
 if __name__ == "__main__":
